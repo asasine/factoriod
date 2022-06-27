1c613823d0240dd3aa386d8399d0648383f9c4c0
Adam Sasine <adam.sasine@gmail.com>
2022-06-27 07:15:55 -0400

    Added .tar.xz extraction

--- src/Factoriod.Fetcher/Program.cs
+++ src/Factoriod.Fetcher/Program.cs
@@ -2,6 +2,8 @@
 using System.IO;
 using System.Net.Http;
 using System.Threading.Tasks;
+using SharpCompress.Common;
+using SharpCompress.Readers;
 
 namespace Factoriod.Fetcher
 {
@@ -18,12 +20,38 @@ namespace Factoriod.Fetcher
                 return;
             }
             
-            var outputDirectory = new DirectoryInfo(Path.Join(Path.GetTempPath(), "factoriod", "downloads"));
+            
+            var outputDirectory = new DirectoryInfo(Path.Join(Path.GetTempPath(), "factoriod"));
+            var downloadsDirectory = new DirectoryInfo(Path.Join(outputDirectory.FullName, "downloads"));
             outputDirectory.Create();
-            Console.WriteLine($"Downloading {version} to {outputDirectory}");
+            Console.WriteLine($"Downloading {version} to {downloadsDirectory}");
             var releaseFetcher = new ReleaseFetcher(client);
-            var outputFile = await releaseFetcher.DownloadToAsync(version, Distro.Linux64, outputDirectory);
+            var outputFile = await releaseFetcher.DownloadToAsync(version, Distro.Linux64, downloadsDirectory);
             Console.WriteLine($"Downloaded to {outputFile}");
+
+            if (outputFile.Directory == null)
+            {
+                Console.WriteLine($"Could not find directory of {outputFile}");
+                return;
+            }
+
+            var extractedFiles = Extract(outputFile, outputFile.Directory);
+            Console.WriteLine($"Extracted to {extractedFiles}");
+        }
+
+        private static DirectoryInfo Extract(FileInfo input, DirectoryInfo output)
+        {
+            Console.WriteLine($"Extracting {input} to {output}");
+            output.Create();
+            using var inputStream = input.OpenRead();
+            using var reader = ReaderFactory.Open(inputStream);
+            reader.WriteAllToDirectory(output.FullName, new ExtractionOptions
+            {
+                ExtractFullPath = true,
+                Overwrite = true
+            });
+
+            return new DirectoryInfo(Path.Join(output.FullName, "factorio"));
         }
     }
 }
