90e51565e755f18727665748b9e7cf836230c2eb
Adam Sasine <adam.sasine@gmail.com>
2023-06-25 06:00:30 -0400

    Removed copying of mod-list.json to mod directory

--- src/Factoriod.Daemon/FactorioProcess.cs
+++ src/Factoriod.Daemon/FactorioProcess.cs
@@ -736,9 +736,9 @@ public sealed class FactorioProcess : RestartableBackgroundService
         var modsRootDirectory = this.options.GetModsRootDirectory();
         modsRootDirectory.Create();
         modsRootDirectory.Refresh();
-        var runtimeModListJsonPath = Path.Combine(modsRootDirectory.FullName, "mod-list.json");
-        this.logger.LogTrace("Copying mod list from {source} to {destination}", modListJson.FullName, runtimeModListJsonPath);
-        modListJson.CopyTo(runtimeModListJsonPath, overwrite: true);
+        //var runtimeModListJsonPath = Path.Combine(modsRootDirectory.FullName, "mod-list.json");
+        //this.logger.LogTrace("Copying mod list from {source} to {destination}", modListJson.FullName, runtimeModListJsonPath);
+        //modListJson.CopyTo(runtimeModListJsonPath, overwrite: true);
         AddArgumentIfDirectoryExists(arguments, "--mod-directory", modsRootDirectory);
 
         // ensure all mods are downloaded
